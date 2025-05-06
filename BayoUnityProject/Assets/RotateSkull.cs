b41d15fb095426ef9ae904aca1cc9430f576f088
jackv <jackv@MSI>
2025-05-06 13:33:28 -0400

    Unity project hopefully added to repo

---
+++ BayoUnityProject/Assets/RotateSkull.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RotateSkull : MonoBehaviour
+{
+    private ParticleSystem ps;
+    private GameObject skullObj;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        skullObj = gameObject.transform.Find("skull").gameObject;
+        //id = mat.GetTexturePropertyNameIDs()[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = skullObj.transform.rotation.eulerAngles.x;
+        float y = skullObj.transform.rotation.eulerAngles.y;
+        skullObj.transform.eulerAngles = new Vector3(x, y, ps.main.startRotationZ.Evaluate(0.5f));
+    }
+}
